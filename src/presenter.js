34e705eb6fbb88f9170fc52852fa876298be1330
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2023-08-24 09:24:30 -0400

    Tarea Totalizador cantidad

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,10 @@
-import operaciones from "./calculadora";
+//import fizzbuzz from "./fizzbuzz"
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#calculadora-form");
-const div = document.querySelector("#resultados-div");
+const cantidad = document.querySelector("#cant-item");
+const form = document.querySelector("#fb-form");
+const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p><i>Resultado Suma:</i> " + operaciones.sumar(firstNumber, secondNumber) + "&nbsp; &nbsp; &nbsp; &nbsp; <i>Resultado Multiplicación:</i> " + operaciones.multiplicar(firstNumber, secondNumber) +"</p>";
+  div.innerHTML += "<p>La cantidad de items ingresada es: "+ cantidad.value +"</p>";
 });
